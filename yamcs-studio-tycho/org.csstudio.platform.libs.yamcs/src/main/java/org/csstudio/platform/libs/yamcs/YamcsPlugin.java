512061cd57bbce7229922a509860b524a18c318b
Fabian Diet <fabian.diet@spaceapplications.com>
2015-03-27 08:13:42 +0100

    Offload mdb loading to bundlelistener instead of activator-start()
    Previous code was causing problems when no connection could
    be made, since bundle activation would timeout after 5s, and
    even though the calls ought to be async, they still blocked the
    whole thing.

--- yamcs-studio-tycho/org.csstudio.platform.libs.yamcs/src/main/java/org/csstudio/platform/libs/yamcs/YamcsPlugin.java
+++ yamcs-studio-tycho/org.csstudio.platform.libs.yamcs/src/main/java/org/csstudio/platform/libs/yamcs/YamcsPlugin.java
@@ -16,7 +16,10 @@ import java.util.logging.Logger;
 import org.csstudio.platform.libs.yamcs.web.RESTClientEndpoint;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleEvent;
+import org.osgi.framework.BundleListener;
 import org.yamcs.protostuff.NamedObjectId;
 import org.yamcs.protostuff.RESTService;
 import org.yamcs.protostuff.RESTService.ResponseHandler;
@@ -29,120 +32,147 @@ import org.yamcs.xtce.XtceDb;
 
 public class YamcsPlugin extends AbstractUIPlugin {
 
-    public static final String PLUGIN_ID = "org.csstudio.utility.platform.libs.yamcs";
-    private static final Logger log = Logger.getLogger(YamcsPlugin.class.getName());
+	public static final String PLUGIN_ID = "org.csstudio.utility.platform.libs.yamcs";
+	private static final Logger log = Logger.getLogger(YamcsPlugin.class.getName());
 
-    // The shared instance
-    private static YamcsPlugin plugin;
+	// The shared instance
+	private static YamcsPlugin plugin;
+	private BundleListener bundleListener;
 
-    private RESTService restService;
+	private RESTService restService;
 
-    private Set<MDBContextListener> mdbListeners = new HashSet<>();
+	private Set<MDBContextListener> mdbListeners = new HashSet<>();
 
-    private List<NamedObjectId> parameterIds = Collections.emptyList();
-    private CountDownLatch parametersLoaded = new CountDownLatch(1);
+	private List<NamedObjectId> parameterIds = Collections.emptyList();
+	private CountDownLatch parametersLoaded = new CountDownLatch(1);
 
-    private Collection<MetaCommand> commands = Collections.emptyList();
-    private CountDownLatch commandsLoaded = new CountDownLatch(1);
+	private Collection<MetaCommand> commands = Collections.emptyList();
+	private CountDownLatch commandsLoaded = new CountDownLatch(1);
 
-    public RESTService getRESTService() {
-        return restService;
-    }
+	public RESTService getRESTService() {
+		return restService;
+	}
 
-    @Override
-    public void start(BundleContext context) throws Exception {
-        super.start(context);
-        plugin = this;
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
 
-        log.info("yamcs plugin starting..");
-        String yamcsHost = YamcsPlugin.getDefault().getPreferenceStore().getString("yamcs_host");
-        int yamcsPort = YamcsPlugin.getDefault().getPreferenceStore().getInt("yamcs_port");
-        String yamcsInstance = YamcsPlugin.getDefault().getPreferenceStore().getString("yamcs_instance");
-        restService = new RESTClientEndpoint(new YamcsConnectionProperties(yamcsHost, yamcsPort, yamcsInstance));
-        fetchInitialMdbAsync();
-    }
+		String yamcsHost = YamcsPlugin.getDefault().getPreferenceStore().getString("yamcs_host");
+		int yamcsPort = YamcsPlugin.getDefault().getPreferenceStore().getInt("yamcs_port");
+		String yamcsInstance = YamcsPlugin.getDefault().getPreferenceStore().getString("yamcs_instance");
+		restService = new RESTClientEndpoint(new YamcsConnectionProperties(yamcsHost, yamcsPort, yamcsInstance));
 
-    /**
-     * Returns the MDB namespace as defined in the user preferences.
-     */
-    public String getMdbNamespace() {
-        return YamcsPlugin.getDefault().getPreferenceStore().getString("mdb_namespace");
-    }
+		// Only load MDB once bundle has been fully started
+		bundleListener = new BundleListener() {
+			@Override
+			public void bundleChanged(BundleEvent event) {
+				if (event.getBundle() == getBundle() && event.getType() == BundleEvent.STARTED) {
+					// Extra check, bundle may have been shut down between the time this event was queued and now
+					if (getBundle().getState() == Bundle.ACTIVE) {
+						log.info("loading mdb async?..");
+						fetchInitialMdbAsync();
+						log.info("after fetch async");
+					}
+				}
+			}
+		};
+		context.addBundleListener(bundleListener); // This also gets removed in stop()
 
-    private void fetchInitialMdbAsync() {
-        // Load list of parameters
-        RestListAvailableParametersRequest req = new RestListAvailableParametersRequest();
-        req.setNamespacesList(Arrays.asList(getMdbNamespace()));
-        restService.listAvailableParameters(req, new ResponseHandler<RestListAvailableParametersResponse>() {
-            @Override
-            public void onMessage(RestListAvailableParametersResponse response) {
-                Display.getDefault().asyncExec(() -> {
-                    parameterIds = response.getIdsList();
-                    for (MDBContextListener l : mdbListeners) {
-                        l.onParametersChanged(parameterIds);
-                    }
-                    parametersLoaded.countDown();
-                });
-            }
+		System.out.println("At least we got to the end of the start()");
+	}
 
-            @Override
-            public void onFault(Throwable t) {
-                log.log(Level.SEVERE, "Could not fetch available yamcs parameters", t);
-            }
-        });
+	/**
+	 * Returns the MDB namespace as defined in the user preferences.
+	 */
+	public String getMdbNamespace() {
+		return YamcsPlugin.getDefault().getPreferenceStore().getString("mdb_namespace");
+	}
 
-        // Load commands
-        RestDumpRawMdbRequest dumpRequest = new RestDumpRawMdbRequest();
-        restService.dumpRawMdb(dumpRequest, new ResponseHandler<RestDumpRawMdbResponse>() {
-            @Override
-            public void onMessage(RestDumpRawMdbResponse response) {
-                // In-memory :-( no easy way to get ByteString as inputstream (?)
-                byte[] barray = response.getRawMdb().toByteArray();
-                try (ObjectInputStream oin = new ObjectInputStream(new ByteArrayInputStream(barray))) {
-                    XtceDb mdb = (XtceDb) oin.readObject();
-                    Display.getDefault().asyncExec(() -> {
-                        commands = mdb.getMetaCommands();
-                        for (MDBContextListener l : mdbListeners) {
-                            l.onCommandsChanged(commands);
-                        }
-                        commandsLoaded.countDown();
-                    });
-                } catch (IOException | ClassNotFoundException e) {
-                    log.log(Level.SEVERE, "Could not deserialize mdb", e);
-                }
-            }
+	private void fetchInitialMdbAsync() {
+		// Load list of parameters
+		RestListAvailableParametersRequest req = new RestListAvailableParametersRequest();
+		req.setNamespacesList(Arrays.asList(getMdbNamespace()));
+		restService.listAvailableParameters(req, new ResponseHandler<RestListAvailableParametersResponse>() {
+			@Override
+			public void onMessage(RestListAvailableParametersResponse response) {
+				Display.getDefault().asyncExec(() -> {
+					parameterIds = response.getIdsList();
+					for (MDBContextListener l : mdbListeners) {
+						l.onParametersChanged(parameterIds);
+					}
+					parametersLoaded.countDown();
+				});
+			}
 
-            @Override
-            public void onFault(Throwable t) {
-                log.log(Level.SEVERE, "Could not fetch available yamcs commands", t);
-            }
-        });
-    }
+			@Override
+			public void onFault(Throwable t) {
+				log.log(Level.SEVERE, "Could not fetch available yamcs parameters", t);
+			}
+		});
 
-    public void addMdbListener(MDBContextListener listener) {
-        mdbListeners.add(listener);
-    }
+		System.out.println("paramaters.done, continuing to raw dump");
 
-    @Override
-    public void stop(BundleContext context) throws Exception {
-        plugin = null;
-        restService.shutdown();
-        YRegistrar.getInstance().shutdown();
-        super.stop(context);
-    }
+		// Load commands
+		RestDumpRawMdbRequest dumpRequest = new RestDumpRawMdbRequest();
+		restService.dumpRawMdb(dumpRequest, new ResponseHandler<RestDumpRawMdbResponse>() {
+			@Override
+			public void onMessage(RestDumpRawMdbResponse response) {
+				// In-memory :-( no easy way to get ByteString as inputstream (?)
+				byte[] barray = response.getRawMdb().toByteArray();
+				try (ObjectInputStream oin = new ObjectInputStream(new ByteArrayInputStream(barray))) {
+					XtceDb mdb = (XtceDb) oin.readObject();
+					Display.getDefault().asyncExec(() -> {
+						commands = mdb.getMetaCommands();
+						for (MDBContextListener l : mdbListeners) {
+							l.onCommandsChanged(commands);
+						}
+						commandsLoaded.countDown();
+					});
+				} catch (IOException | ClassNotFoundException e) {
+					log.log(Level.SEVERE, "Could not deserialize mdb", e);
+				}
+			}
 
-    public static YamcsPlugin getDefault() {
-        return plugin;
-    }
+			@Override
+			public void onFault(Throwable t) {
+				log.log(Level.SEVERE, "Could not fetch available yamcs commands", t);
+			}
+		});
 
-    /**
-     * Return the available parameters
-     */
-    public List<NamedObjectId> getParameterIds() {
-        return parameterIds;
-    }
+		System.out.println("raw dump done, continuing to commands");
+	}
 
-    public Collection<MetaCommand> getCommands() {
-        return commands;
-    }
+	public void addMdbListener(MDBContextListener listener) {
+		mdbListeners.add(listener);
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		try {
+			if (bundleListener != null) {
+				context.removeBundleListener(bundleListener);
+			}
+			plugin = null;
+			restService.shutdown();
+			YRegistrar.getInstance().shutdown();
+		} finally {
+			super.stop(context);
+		}
+	}
+
+	public static YamcsPlugin getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Return the available parameters
+	 */
+	public List<NamedObjectId> getParameterIds() {
+		return parameterIds;
+	}
+
+	public Collection<MetaCommand> getCommands() {
+		return commands;
+	}
 }
--- yamcs-studio-tycho/org.csstudio.platform.libs.yamcs/src/main/java/org/csstudio/platform/libs/yamcs/web/RESTClientEndpoint.java
+++ yamcs-studio-tycho/org.csstudio.platform.libs.yamcs/src/main/java/org/csstudio/platform/libs/yamcs/web/RESTClientEndpoint.java
@@ -58,169 +58,191 @@ import org.yamcs.protostuff.RestValidateCommandResponse;
  */
 public class RESTClientEndpoint implements RESTService {
 
-    private YamcsConnectionProperties yprops;
-    private EventLoopGroup group = new NioEventLoopGroup(1);
+	private YamcsConnectionProperties yprops;
+	private EventLoopGroup group = new NioEventLoopGroup(1);
 
-    private AtomicInteger cmdClientId = new AtomicInteger(1); // Reset for every application restart
+	private AtomicInteger cmdClientId = new AtomicInteger(1); // Reset for every application restart
 
-    // Reuse the same buffer for serializing multiple post requests
-    private LinkedBuffer contentBuffer = LinkedBuffer.allocate(4096); // Increase value when requests are getting too big
+	// Reuse the same buffer for serializing multiple post requests
+	private LinkedBuffer contentBuffer = LinkedBuffer.allocate(4096); // Increase value when requests are getting too big
 
-    public RESTClientEndpoint(YamcsConnectionProperties yprops) {
-        this.yprops = yprops;
-    }
+	public RESTClientEndpoint(YamcsConnectionProperties yprops) {
+		this.yprops = yprops;
+	}
 
-    @Override
-    public void replay(ReplayRequest request, ResponseHandler<RestReplayResponse> responseHandler) {
-        // TODO Auto-generated method stub
-    }
+	@Override
+	public void replay(ReplayRequest request, ResponseHandler<RestReplayResponse> responseHandler) {
+		// TODO Auto-generated method stub
+	}
 
-    @Override
-    public void validateCommand(RestValidateCommandRequest request, ResponseHandler<RestValidateCommandResponse> responseHandler) {
-        URI uri = yprops.webResourceURI("/api/commanding/validate");
-        doPOST(uri, request, new RestValidateCommandResponse(), responseHandler);
-    }
+	@Override
+	public void validateCommand(RestValidateCommandRequest request, ResponseHandler<RestValidateCommandResponse> responseHandler) {
+		URI uri = yprops.webResourceURI("/api/commanding/validate");
+		doPOST(uri, request, new RestValidateCommandResponse(), responseHandler);
+	}
 
-    @Override
-    public void sendCommand(RestSendCommandRequest request, ResponseHandler<RestSendCommandResponse> responseHandler) {
-        URI uri = yprops.webResourceURI("/api/commanding/send");
-        String origin;
-        try {
-            origin = InetAddress.getLocalHost().getHostName();
-        } catch (UnknownHostException e) {
-            origin = "Unknown";
-        }
-        for (RestCommandType cmd : request.getCommandsList()) {
-            cmd.setSequenceNumber(cmdClientId.getAndIncrement());
-            cmd.setOrigin(origin);
-        }
-        doPOST(uri, request, new RestSendCommandResponse(), responseHandler);
-    }
+	@Override
+	public void sendCommand(RestSendCommandRequest request, ResponseHandler<RestSendCommandResponse> responseHandler) {
+		URI uri = yprops.webResourceURI("/api/commanding/send");
+		String origin;
+		try {
+			origin = InetAddress.getLocalHost().getHostName();
+		} catch (UnknownHostException e) {
+			origin = "Unknown";
+		}
+		for (RestCommandType cmd : request.getCommandsList()) {
+			cmd.setSequenceNumber(cmdClientId.getAndIncrement());
+			cmd.setOrigin(origin);
+		}
+		doPOST(uri, request, new RestSendCommandResponse(), responseHandler);
+	}
 
-    @Override
-    public void listAvailableParameters(RestListAvailableParametersRequest request, ResponseHandler<RestListAvailableParametersResponse> responseHandler) {
-        URI uri = yprops.webResourceURI("/api/mdb/parameters");
-        // TODO post for now, but should become get with request body
-        doPOST(uri, request, new RestListAvailableParametersResponse(), responseHandler);
-    }
+	@Override
+	public void listAvailableParameters(RestListAvailableParametersRequest request, ResponseHandler<RestListAvailableParametersResponse> responseHandler) {
+		URI uri = yprops.webResourceURI("/api/mdb/parameters");
+		// TODO post for now, but should become get with request body
+		doPOST(uri, request, new RestListAvailableParametersResponse(), responseHandler);
+	}
 
-    @Override
-    public void dumpRawMdb(RestDumpRawMdbRequest request, ResponseHandler<RestDumpRawMdbResponse> responseHandler) {
-        URI uri = yprops.webResourceURI("/api/mdb/dump");
-        doGET(uri, new RestDumpRawMdbResponse(), responseHandler);
-    }
+	@Override
+	public void dumpRawMdb(RestDumpRawMdbRequest request, ResponseHandler<RestDumpRawMdbResponse> responseHandler) {
+		URI uri = yprops.webResourceURI("/api/mdb/dump");
+		doGET(uri, new RestDumpRawMdbResponse(), responseHandler);
+	}
 
-    private <T extends Message<T>> void doGET(URI uri, T target, ResponseHandler<T> handler) {
-        try {
-            Bootstrap b = new Bootstrap();
-            b.group(group).channel(NioSocketChannel.class).handler(new ChannelInitializer<SocketChannel>() {
-                @Override
-                protected void initChannel(SocketChannel ch) throws Exception {
-                    ChannelPipeline p = ch.pipeline();
-                    p.addLast(new HttpClientCodec());
-                    p.addLast(new HttpObjectAggregator(1048576));
-                    p.addLast(new SimpleChannelInboundHandler<FullHttpResponse>() {
-                        @Override
-                        public void channelRead0(ChannelHandlerContext ctx, FullHttpResponse response) throws Exception {
-                            //ProtobufIOUtil.mergeFrom(new ByteBufInputStream(response.content()), target, target.cachedSchema());
-                            JsonIOUtil.mergeFrom(new ByteBufInputStream(response.content()), target, target.cachedSchema(), false);
-                            ctx.close();
-                            handler.onMessage(target);
-                        }
+	private <T extends Message<T>> void doGET(URI uri, T target, ResponseHandler<T> handler) {
+		try {
+			Bootstrap b = new Bootstrap();
+			b.group(group).channel(NioSocketChannel.class).handler(new ChannelInitializer<SocketChannel>() {
+				@Override
+				protected void initChannel(SocketChannel ch) throws Exception {
+					ChannelPipeline p = ch.pipeline();
+					p.addLast(new HttpClientCodec());
+					p.addLast(new HttpObjectAggregator(1048576));
+					p.addLast(new SimpleChannelInboundHandler<FullHttpResponse>() {
+						@Override
+						public void channelRead0(ChannelHandlerContext ctx, FullHttpResponse response) throws Exception {
+							//ProtobufIOUtil.mergeFrom(new ByteBufInputStream(response.content()), target, target.cachedSchema());
+							JsonIOUtil.mergeFrom(new ByteBufInputStream(response.content()), target, target.cachedSchema(), false);
+							ctx.close();
+							handler.onMessage(target);
+						}
 
-                        @Override
-                        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-                            cause.printStackTrace();
-                            ctx.close();
-                            handler.onFault(cause);
-                        }
-                    });
-                }
-            });
+						@Override
+						public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+							cause.printStackTrace();
+							ctx.close();
+							handler.onFault(cause);
+						}
+					});
+				}
+			});
 
-            Channel ch = b.connect(uri.getHost(), uri.getPort()).sync().channel();
-            HttpRequest request = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, uri.getRawPath());
-            request.headers().set(HttpHeaders.Names.HOST, uri.getHost());
-            request.headers().set(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.CLOSE);
-            request.headers().set(HttpHeaders.Names.ACCEPT, "application/json");
-            ch.writeAndFlush(request);
-            ch.closeFuture().sync();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+			Channel ch = b.connect(uri.getHost(), uri.getPort()).sync().channel();
+			HttpRequest request = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, uri.getRawPath());
+			request.headers().set(HttpHeaders.Names.HOST, uri.getHost());
+			request.headers().set(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.CLOSE);
+			request.headers().set(HttpHeaders.Names.ACCEPT, "application/json");
+			ch.writeAndFlush(request);
+			ch.closeFuture().sync();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 
-    private <S extends Message<S>, T extends Message<T>> void doPOST(URI uri, S msg, T target, ResponseHandler<T> handler) {
-        try {
-            Bootstrap b = new Bootstrap();
-            b.group(group).channel(NioSocketChannel.class).handler(new ChannelInitializer<SocketChannel>() {
-                @Override
-                protected void initChannel(SocketChannel ch) throws Exception {
-                    ChannelPipeline p = ch.pipeline();
-                    p.addLast(new HttpClientCodec());
-                    p.addLast(new HttpObjectAggregator(1048576));
-                    p.addLast(new SimpleChannelInboundHandler<FullHttpResponse>() {
-                        @Override
-                        public void channelRead0(ChannelHandlerContext ctx, FullHttpResponse response) throws Exception {
-                            JsonIOUtil.mergeFrom(new ByteBufInputStream(response.content()), target, target.cachedSchema(), false);
-                            //ProtobufIOUtil.mergeFrom(new ByteBufInputStream(response.content()), target, target.cachedSchema());
-                            ctx.close();
-                            handler.onMessage(target);
-                        }
+	private <S extends Message<S>, T extends Message<T>> void doPOST(URI uri, S msg, T target, ResponseHandler<T> handler) {
+		try {
+			Bootstrap b = new Bootstrap();
+			b.group(group).channel(NioSocketChannel.class).handler(new ChannelInitializer<SocketChannel>() {
+				@Override
+				protected void initChannel(SocketChannel ch) throws Exception {
+					ChannelPipeline p = ch.pipeline();
+					p.addLast(new HttpClientCodec());
+					p.addLast(new HttpObjectAggregator(1048576));
+					p.addLast(new SimpleChannelInboundHandler<FullHttpResponse>() {
+						@Override
+						public void channelRead0(ChannelHandlerContext ctx, FullHttpResponse response) throws Exception {
+							JsonIOUtil.mergeFrom(new ByteBufInputStream(response.content()), target, target.cachedSchema(), false);
+							//ProtobufIOUtil.mergeFrom(new ByteBufInputStream(response.content()), target, target.cachedSchema());
+							ctx.close();
+							handler.onMessage(target);
+						}
 
-                        @Override
-                        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-                            cause.printStackTrace();
-                            ctx.close();
-                            handler.onFault(cause);
-                        }
-                    });
-                }
-            });
+						@Override
+						public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+							cause.printStackTrace();
+							ctx.close();
+							handler.onFault(cause);
+						}
+					});
+				}
+			});
 
-            Channel ch = b.connect(uri.getHost(), uri.getPort()).sync().channel();
-            FullHttpRequest request = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.POST, uri.getRawPath());
-            request.headers().set(HttpHeaders.Names.HOST, uri.getHost());
-            request.headers().set(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.CLOSE);
-            request.headers().set(HttpHeaders.Names.CONTENT_TYPE, "application/octet-stream");
-            request.headers().set(HttpHeaders.Names.ACCEPT, "application/json");
-            // Unclear why we need this contentbuffer. protobufioutil seems to first write there, and only afterwards stream out
-            ProtobufIOUtil.writeTo(new ByteBufOutputStream(request.content()), msg, msg.cachedSchema(), contentBuffer);
-            contentBuffer.clear(); // Prepare for next usage
-            request.headers().set(HttpHeaders.Names.CONTENT_LENGTH, request.content().readableBytes());
-            ch.writeAndFlush(request);
-            ch.closeFuture().sync();
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+			Channel ch = b.connect(uri.getHost(), uri.getPort()).sync().channel();
+			FullHttpRequest request = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.POST, uri.getRawPath());
+			request.headers().set(HttpHeaders.Names.HOST, uri.getHost());
+			request.headers().set(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.CLOSE);
+			request.headers().set(HttpHeaders.Names.CONTENT_TYPE, "application/octet-stream");
+			request.headers().set(HttpHeaders.Names.ACCEPT, "application/json");
+			// Unclear why we need this contentbuffer. protobufioutil seems to first write there, and only afterwards stream out
+			ProtobufIOUtil.writeTo(new ByteBufOutputStream(request.content()), msg, msg.cachedSchema(), contentBuffer);
+			contentBuffer.clear(); // Prepare for next usage
+			request.headers().set(HttpHeaders.Names.CONTENT_LENGTH, request.content().readableBytes());
+			ch.writeAndFlush(request);
+			ch.closeFuture().sync();
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 
-    @Override
-    public void shutdown() {
-        group.shutdownGracefully();
-    }
+	@Override
+	public void shutdown() {
+		group.shutdownGracefully();
+	}
 
-    public static void main(String... args) throws IOException {
-        RestListAvailableParametersRequest req = new RestListAvailableParametersRequest();
-        req.setNamespacesList(Arrays.asList("MDB:OPS Name"));
-        JsonIOUtil.writeTo(System.out, req, req.cachedSchema(), false);
+	public static void main(String... args) throws IOException {
+		RestListAvailableParametersRequest req = new RestListAvailableParametersRequest();
+		req.setNamespacesList(Arrays.asList("MDB:OPS Name"));
+		JsonIOUtil.writeTo(System.out, req, req.cachedSchema(), false);
 
-        YamcsConnectionProperties yprops = new YamcsConnectionProperties("localhost", 8090, "simulator");
-        RESTClientEndpoint client = new RESTClientEndpoint(yprops);
-        client.listAvailableParameters(req, new ResponseHandler<RestListAvailableParametersResponse>() {
-            @Override
-            public void onMessage(RestListAvailableParametersResponse msg) {
-                System.out.println("got back msg "+msg);
-                client.shutdown();
-            }
+		YamcsConnectionProperties yprops = new YamcsConnectionProperties("localhost", 8090, "simulator");
+		RESTClientEndpoint client = new RESTClientEndpoint(yprops);
+		try {
+			client.listAvailableParameters(req, new ResponseHandler<RestListAvailableParametersResponse>() {
+				@Override
+				public void onMessage(RestListAvailableParametersResponse msg) {
+					System.out.println("got back msg "+msg);
+					client.shutdown();
+				}
 
-            @Override
-            public void onFault(Throwable t) {
-                System.out.println("got back fault "+t);
-                t.printStackTrace();
-                client.shutdown();
-            }
-        });
+				@Override
+				public void onFault(Throwable t) {
+					System.out.println("got back fault "+t);
+					t.printStackTrace();
+					client.shutdown();
+				}
+			});
+			System.out.println("after list params");
 
-    }
+			client.listAvailableParameters(req, new ResponseHandler<RestListAvailableParametersResponse>() {
+				@Override
+				public void onMessage(RestListAvailableParametersResponse msg) {
+					System.out.println("got back msg "+msg);
+					client.shutdown();
+				}
+
+				@Override
+				public void onFault(Throwable t) {
+					System.out.println("got back fault "+t);
+					t.printStackTrace();
+					client.shutdown();
+				}
+			});
+
+			System.out.println("after list cmd");
+		} finally {
+			System.out.println("shutting down..");
+			client.shutdown();
+		}
+	}
 }
