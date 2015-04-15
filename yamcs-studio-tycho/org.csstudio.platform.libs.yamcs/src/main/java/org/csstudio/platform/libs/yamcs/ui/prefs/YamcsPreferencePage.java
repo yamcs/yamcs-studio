cd302cbe99d94b3d622ba320c2b8812ed4a29f18
Fabian Diet <fabian.diet@spaceapplications.com>
2015-04-15 16:47:50 +0200

    Fix issue where ip's were not accepted

--- yamcs-studio-tycho/org.csstudio.platform.libs.yamcs/src/main/java/org/csstudio/platform/libs/yamcs/ui/prefs/YamcsPreferencePage.java
+++ yamcs-studio-tycho/org.csstudio.platform.libs.yamcs/src/main/java/org/csstudio/platform/libs/yamcs/ui/prefs/YamcsPreferencePage.java
@@ -10,14 +10,13 @@ import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
 /**
- * This class represents a preference page that is contributed to the
- * Preferences dialog. By subclassing <samp>FieldEditorPreferencePage</samp>, we
- * can use the field support built into JFace that allows us to create a page
- * that is small and knows how to save, restore and apply itself.
+ * This class represents a preference page that is contributed to the Preferences dialog. By
+ * subclassing <samp>FieldEditorPreferencePage</samp>, we can use the field support built into JFace
+ * that allows us to create a page that is small and knows how to save, restore and apply itself.
  * <p>
- * This page is used to modify preferences only. They are stored in the
- * preference store that belongs to the main plug-in class. That way,
- * preferences can be accessed directly via the preference store.
+ * This page is used to modify preferences only. They are stored in the preference store that
+ * belongs to the main plug-in class. That way, preferences can be accessed directly via the
+ * preference store.
  */
 public class YamcsPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 
@@ -36,9 +35,9 @@ public class YamcsPreferencePage extends FieldEditorPreferencePage implements IW
     }
 
     /**
-     * Creates the field editors. Field editors are abstractions of the common
-     * GUI blocks needed to manipulate various types of preferences. Each field
-     * editor knows how to save and restore itself.
+     * Creates the field editors. Field editors are abstractions of the common GUI blocks needed to
+     * manipulate various types of preferences. Each field editor knows how to save and restore
+     * itself.
      */
     @Override
     public void createFieldEditors() {
@@ -66,8 +65,8 @@ public class YamcsPreferencePage extends FieldEditorPreferencePage implements IW
         }
         String yamcsHostText = yamcsHost.getStringValue();
         String yamcsInstanceText = yamcsInstance.getStringValue();
-        if (!(yamcsHostText.trim().matches("[a-zA-Z0-9_]+")) || !(yamcsInstanceText.trim().matches("[a-zA-Z0-9_]+"))) {
-            setErrorMessage("Host and instance must contain alphanumerics and _ with no white space");
+        if (!(yamcsHostText.trim().matches("[a-zA-Z\\-0-9_]+")) || !(yamcsInstanceText.trim().matches("[a-zA-Z0-9_]+"))) {
+            setErrorMessage("Not a valid host name");
             setValid(false);
         } else {
             setErrorMessage(null);
