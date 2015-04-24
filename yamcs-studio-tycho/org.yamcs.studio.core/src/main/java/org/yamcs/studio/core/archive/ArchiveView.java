9f6d350aa963bf43c507846b092cae7a79afa144
Fabian Diet <fabian.diet@spaceapplications.com>
2015-04-24 14:42:45 +0200

    Add placeholder for Archive View

---
+++ yamcs-studio-tycho/org.yamcs.studio.core/src/main/java/org/yamcs/studio/core/archive/ArchiveView.java
@@ -0,0 +1,20 @@
+package org.yamcs.studio.core.archive;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class ArchiveView extends ViewPart {
+
+    @Override
+    public void createPartControl(Composite parent) {
+    }
+
+    @Override
+    public void setFocus() {
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+}
