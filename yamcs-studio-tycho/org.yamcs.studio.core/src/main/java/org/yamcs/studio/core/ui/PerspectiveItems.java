8b3c50d455a0dcdb5bbcfb9d55532cad45d75237
Fabian Diet <fabian.diet@spaceapplications.com>
2015-05-09 17:45:12 +0200

    Work around identical opibuilder/opirunner perspective icon

--- yamcs-studio-tycho/org.yamcs.studio.core/src/main/java/org/yamcs/studio/core/ui/PerspectiveItems.java
+++ yamcs-studio-tycho/org.yamcs.studio.core/src/main/java/org/yamcs/studio/core/ui/PerspectiveItems.java
@@ -5,9 +5,11 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
+import org.csstudio.ui.util.CustomMediaFactory;
 import org.eclipse.core.commands.Command;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.action.IContributionItem;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.IPerspectiveDescriptor;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
@@ -23,6 +25,10 @@ public class PerspectiveItems extends CompoundContributionItem {
 
     public static final String OPEN_PERSPECTIVE_COMMAND = "org.yamcs.studio.core.ui.commands.openPerspectiveCommand";
     private static final List<String> SUPPORTED_PERSPECTIVES = Arrays.asList(IDs.OPI_EDITOR_PERSPECTIVE, IDs.OPI_RUNTIME_PERSPECTIVE);
+
+    // The runtime perspective image is identical to the builder in our current cs-studio dependencies, so override.
+    private static ImageDescriptor OPI_RUNTIME_IMAGE = CustomMediaFactory.getInstance()
+            .getImageDescriptorFromPlugin(IDs.OPI_RUNTIME_PERSPECTIVE, "icons/OPIRunner.png"); //$NON-NLS-1$
 
     @Override
     public IContributionItem[] getContributionItems() {
@@ -50,7 +56,10 @@ public class PerspectiveItems extends CompoundContributionItem {
         params.put(RadioState.PARAMETER_ID, perspective.getId());
 
         itemParameter.label = perspective.getLabel();
-        itemParameter.icon = perspective.getImageDescriptor();
+        if (perspective.getId().equals(IDs.OPI_RUNTIME_PERSPECTIVE))
+            itemParameter.icon = OPI_RUNTIME_IMAGE;
+        else
+            itemParameter.icon = perspective.getImageDescriptor();
         itemParameter.tooltip = perspective.getDescription();
         itemParameter.parameters = params;
 
