5865b18efc2ca1edc53414ae8c1cafab42d185a7
Fabian Diet <fabian.diet@spaceapplications.com>
2015-05-11 22:57:01 +0200

    Remove unwanted preference pages. Re-categorize some others

--- yamcs-studio-tycho/org.yamcs.studio.core/src/main/java/org/yamcs/studio/core/application/YamcsStudioWorkbenchAdvisor.java
+++ yamcs-studio-tycho/org.yamcs.studio.core/src/main/java/org/yamcs/studio/core/application/YamcsStudioWorkbenchAdvisor.java
@@ -2,6 +2,8 @@ package org.yamcs.studio.core.application;
 
 import org.csstudio.startup.application.OpenDocumentEventProcessor;
 import org.csstudio.utility.product.ApplicationWorkbenchAdvisor;
+import org.eclipse.jface.preference.PreferenceManager;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
@@ -19,5 +21,15 @@ public class YamcsStudioWorkbenchAdvisor extends ApplicationWorkbenchAdvisor {
     @Override
     public String getInitialWindowPerspectiveId() {
         return IDs.OPI_RUNTIME_PERSPECTIVE;
+    }
+
+    @Override
+    public void postStartup() {
+        PreferenceManager pm = PlatformUI.getWorkbench().getPreferenceManager();
+        pm.remove("org.eclipse.help.ui.browsersPreferencePage");
+        pm.remove("org.eclipse.team.ui.TeamPreferences");
+        pm.remove("org.csstudio.platform.ui.css.applications");
+        pm.remove("org.csstudio.platform.ui.css.platform");
+        pm.remove("org.csstudio.yamcs.ycl.dsl.YCL");
     }
 }
