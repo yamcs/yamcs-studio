97b47e7f437dada86eab1edfd819bbe315722e5d
Fabian Diet <fabian.diet@spaceapplications.com>
2015-05-15 07:55:42 +0200

    Enable go-to-beginning/end on replays

---
+++ yamcs-studio-tycho/org.yamcs.studio.ui/src/main/java/org/yamcs/studio/ui/handlers/GoToBeginningHandler.java
@@ -0,0 +1,50 @@
+package org.yamcs.studio.ui.handlers;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.yamcs.protobuf.Rest.RestExceptionMessage;
+import org.yamcs.protobuf.YamcsManagement.ProcessorInfo;
+import org.yamcs.protobuf.YamcsManagement.ProcessorRequest;
+import org.yamcs.protobuf.YamcsManagement.ProcessorRequest.Operation;
+import org.yamcs.studio.core.YamcsPlugin;
+import org.yamcs.studio.core.web.ResponseHandler;
+import org.yamcs.studio.core.web.RestClient;
+
+import com.google.protobuf.MessageLite;
+
+public class GoToBeginningHandler extends AbstractHandler {
+
+    private static final Logger log = Logger.getLogger(GoToBeginningHandler.class.getName());
+
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        RestClient restClient = YamcsPlugin.getDefault().getRestClient();
+        String processorName = YamcsPlugin.getDefault().getClientInfo().getProcessorName();
+        ProcessorInfo processorInfo = YamcsPlugin.getDefault().getProcessorInfo(processorName);
+        long seekTime = processorInfo.getReplayRequest().getStart();
+
+        ProcessorRequest req = ProcessorRequest.newBuilder().setOperation(Operation.SEEK).setSeekTime(seekTime).build();
+        restClient.createProcessorRequest(processorName, req, new ResponseHandler() {
+            @Override
+            public void onMessage(MessageLite responseMsg) {
+                if (responseMsg instanceof RestExceptionMessage) {
+                    log.severe("Could not go to beginning of replay range: " + responseMsg);
+                    MessageDialog.openError(HandlerUtil.getActiveShell(event), "Seek Error",
+                            ((RestExceptionMessage) responseMsg).getMsg());
+                }
+            }
+
+            @Override
+            public void onException(Exception e) {
+                log.log(Level.SEVERE, "Could not go to beginning of replay range", e);
+            }
+        });
+        return null;
+    }
+}
