6adddd8eecded7b9e8bd918fec3884f9c8d8580c
Fabian Diet <fabian.diet@spaceapplications.com>
2015-05-30 15:32:09 +0200

    Get rid of AfterWorkbenchBootstrap
    moved content to LifeCycleManager. Needs to be refactored when we move clientinfo
    and processorinfo to websocket.

--- yamcs-studio-tycho/org.yamcs.studio.ui/src/main/java/org/yamcs/studio/ui/application/LifeCycleManager.java
+++ yamcs-studio-tycho/org.yamcs.studio.ui/src/main/java/org/yamcs/studio/ui/application/LifeCycleManager.java
@@ -12,13 +12,20 @@ import org.eclipse.e4.ui.workbench.UIEvents.UILifeCycle;
 import org.eclipse.e4.ui.workbench.lifecycle.PostContextCreate;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.commands.ICommandService;
 import org.eclipse.ui.services.IEvaluationService;
+import org.eclipse.ui.services.ISourceProviderService;
+import org.yamcs.protobuf.YamcsManagement.ClientInfo;
+import org.yamcs.protobuf.YamcsManagement.ProcessorInfo;
+import org.yamcs.protobuf.YamcsManagement.Statistics;
 import org.yamcs.studio.core.ConnectionFailureListener;
+import org.yamcs.studio.core.ProcessorListener;
 import org.yamcs.studio.core.YamcsPlugin;
 import org.yamcs.studio.ui.handlers.ConnectHandler;
+import org.yamcs.studio.ui.processor.ProcessorStateProvider;
 
 @SuppressWarnings("restriction")
 public class LifeCycleManager implements ConnectionFailureListener {
@@ -42,6 +49,34 @@ public class LifeCycleManager implements ConnectionFailureListener {
                 Command cmd = commandService.getCommand("org.eclipse.ui.ToggleCoolbarAction");
                 cmd.executeWithChecks(new ExecutionEvent(cmd, new HashMap<String, String>(), null, evaluationService.getCurrentState()));
                 cmd.executeWithChecks(new ExecutionEvent(cmd, new HashMap<String, String>(), null, evaluationService.getCurrentState()));
+
+                // Listen to processing-info updates
+                doUpdateGlobalProcessingState(PlatformUI.getWorkbench(), null); // Trigger initial state
+                YamcsPlugin.getDefault().addProcessorListener(new ProcessorListener() {
+                    @Override
+                    public void processorUpdated(ProcessorInfo processorInfo) {
+                        updateGlobalProcessingState(processorInfo);
+                    }
+
+                    @Override
+                    public void yProcessorClosed(ProcessorInfo processorInfo) {
+                        updateGlobalProcessingState(processorInfo);
+                    }
+
+                    @Override
+                    public void updateStatistics(Statistics stats) {
+                    }
+
+                    @Override
+                    public void clientUpdated(ClientInfo clientInfo) {
+                        updateGlobalProcessingState(clientInfo);
+                    }
+
+                    @Override
+                    public void clientDisconnected(ClientInfo clientInfo) {
+                        updateGlobalProcessingState(clientInfo);
+                    }
+                });
 
                 // request connection to Yamcs server
                 (new ConnectHandler()).execute(null);
@@ -80,8 +115,8 @@ public class LifeCycleManager implements ConnectionFailureListener {
                 YamcsPlugin.getDefault().disconnect();
                 try {
                     YamcsPlugin.getDefault().switchNode(nextNode);
-                    ;
                 } catch (Exception e) {
+                    log.log(Level.SEVERE, "Could not switch node", e);
                 }
             });
         }
@@ -89,5 +124,36 @@ public class LifeCycleManager implements ConnectionFailureListener {
         {
             YamcsPlugin.getDefault().abortSwitchNode();
         }
+    }
+
+    private void updateGlobalProcessingState(ProcessorInfo processorInfo) {
+        // First update state of various buttons (at the level of the workbench)
+        // (TODO sometimes clientInfo has not been updated yet, that's whey we have the next method too)
+        IWorkbench workbench = PlatformUI.getWorkbench();
+        workbench.getDisplay().asyncExec(() -> {
+            ClientInfo clientInfo = YamcsPlugin.getDefault().getClientInfo();
+            if (clientInfo != null && clientInfo.getProcessorName().equals(processorInfo.getName())) {
+                doUpdateGlobalProcessingState(workbench, processorInfo);
+            }
+        });
+    }
+
+    private void updateGlobalProcessingState(ClientInfo clientInfo) {
+        // TODO Not sure which one of this method or the previous would trigger first, and whether that's deterministic
+        // therefore, just have similar logic here.
+        IWorkbench workbench = PlatformUI.getWorkbench();
+        workbench.getDisplay().asyncExec(() -> {
+            if (YamcsPlugin.getDefault().getClientInfo() != null && clientInfo != null
+                    && clientInfo.getId() == YamcsPlugin.getDefault().getClientInfo().getId()) {
+                ProcessorInfo processorInfo = YamcsPlugin.getDefault().getProcessorInfo(clientInfo.getProcessorName());
+                doUpdateGlobalProcessingState(workbench, processorInfo);
+            }
+        });
+    }
+
+    private void doUpdateGlobalProcessingState(IWorkbench workbench, ProcessorInfo processorInfo) {
+        ISourceProviderService service = (ISourceProviderService) workbench.getService(ISourceProviderService.class);
+        ProcessorStateProvider state = (ProcessorStateProvider) service.getSourceProvider(ProcessorStateProvider.STATE_KEY_PROCESSING);
+        state.updateState(processorInfo);
     }
 }
