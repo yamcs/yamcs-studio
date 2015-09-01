6d9b83b97e1ed789e431a6fcf1da88beba77f4df
Fabian Diet <fabian.diet@spaceapplications.com>
2015-09-01 14:20:45 +0200

    Remove stop time from replay request

--- yamcs-studio-tycho/org.yamcs.studio.ui/src/main/java/org/yamcs/studio/ui/archive/CreateReplayDialog.java
+++ yamcs-studio-tycho/org.yamcs.studio.ui/src/main/java/org/yamcs/studio/ui/archive/CreateReplayDialog.java
@@ -71,10 +71,6 @@ public class CreateReplayDialog extends TitleAreaDialog implements StudioConnect
     private DateTime startTime;
     private Calendar startTimeValue;
 
-    private DateTime stopDate;
-    private DateTime stopTime;
-    private Calendar stopTimeValue;
-
     private TableViewer packetsTable;
     private List<String> packetsValue;
 
@@ -105,17 +101,6 @@ public class CreateReplayDialog extends TitleAreaDialog implements StudioConnect
         restClient = null;
     }
 
-    private void validate() {
-        String errorMessage = null;
-        Calendar start = CreateReplayDialog.toCalendar(startDate, startTime);
-        Calendar stop = CreateReplayDialog.toCalendar(stopDate, stopTime);
-        if (start.after(stop))
-            errorMessage = "Stop has to be greater than start";
-
-        setErrorMessage(errorMessage);
-        getButton(IDialogConstants.OK_ID).setEnabled(errorMessage == null);
-    }
-
     @Override
     protected Control createDialogArea(Composite parent) {
         Composite area = (Composite) super.createDialogArea(parent);
@@ -144,31 +129,10 @@ public class CreateReplayDialog extends TitleAreaDialog implements StudioConnect
         rl.center = true;
         startComposite.setLayout(rl);
         startDate = new DateTime(startComposite, SWT.DATE | SWT.LONG | SWT.DROP_DOWN | SWT.BORDER);
-        startDate.addListener(SWT.Selection, e -> validate());
         startTime = new DateTime(startComposite, SWT.TIME | SWT.LONG | SWT.BORDER);
-        startTime.addListener(SWT.Selection, e -> validate());
         if (startTimeValue != null) {
             startDate.setDate(startTimeValue.get(Calendar.YEAR), startTimeValue.get(Calendar.MONTH), startTimeValue.get(Calendar.DAY_OF_MONTH));
             startTime.setTime(startTimeValue.get(Calendar.HOUR_OF_DAY), startTimeValue.get(Calendar.MINUTE), startTimeValue.get(Calendar.SECOND));
-        }
-
-        lbl = new Label(container, SWT.NONE);
-        lbl.setText("Stop:");
-        Composite stopComposite = new Composite(container, SWT.NONE);
-        rl = new RowLayout();
-        rl.marginLeft = 0;
-        rl.marginTop = 0;
-        rl.marginBottom = 0;
-        rl.center = true;
-        rl.fill = true;
-        stopComposite.setLayout(rl);
-        stopDate = new DateTime(stopComposite, SWT.DATE | SWT.LONG | SWT.DROP_DOWN | SWT.BORDER);
-        stopDate.addListener(SWT.Selection, e -> validate());
-        stopTime = new DateTime(stopComposite, SWT.TIME | SWT.LONG | SWT.BORDER);
-        stopTime.addListener(SWT.Selection, e -> validate());
-        if (stopTimeValue != null) {
-            stopDate.setDate(stopTimeValue.get(Calendar.YEAR), stopTimeValue.get(Calendar.MONTH), stopTimeValue.get(Calendar.DAY_OF_MONTH));
-            stopTime.setTime(stopTimeValue.get(Calendar.HOUR_OF_DAY), stopTimeValue.get(Calendar.MINUTE), stopTimeValue.get(Calendar.SECOND));
         }
 
         /*
@@ -300,8 +264,6 @@ public class CreateReplayDialog extends TitleAreaDialog implements StudioConnect
     public void initialize(TimeInterval interval, List<String> packets, List<String> ppGroups) {
         startTimeValue = TimeEncoding.toCalendar(interval.calculateStart());
         startTimeValue.setTimeZone(TimeCatalogue.getInstance().getTimeZone());
-        stopTimeValue = TimeEncoding.toCalendar(interval.calculateStop());
-        stopTimeValue.setTimeZone(TimeCatalogue.getInstance().getTimeZone());
 
         packetsValue = packets;
         ppValue = ppGroups;
@@ -321,7 +283,7 @@ public class CreateReplayDialog extends TitleAreaDialog implements StudioConnect
         ClientInfo ci = ManagementCatalogue.getInstance().getCurrentClientInfo();
         ReplayRequest.Builder rr = ReplayRequest.newBuilder()
                 .setStart(TimeEncoding.fromCalendar(toCalendar(startDate, startTime)))
-                .setStop(TimeEncoding.fromCalendar(toCalendar(stopDate, stopTime)))
+                .setStop(TimeEncoding.MAX_INSTANT)
                 .setEndAction(EndAction.STOP);
 
         if (prr.getNameFilterCount() > 0)
