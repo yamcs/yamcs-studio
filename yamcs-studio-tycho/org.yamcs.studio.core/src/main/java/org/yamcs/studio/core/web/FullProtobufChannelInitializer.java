2f742ad1c269f80a9ab03c17be5228954d4bf5d7
Mathieu Schmitt <mathieu.schmitt@spaceapplications.com>
2015-11-12 16:10:25 +0100

    increased HTTP message max size

--- yamcs-studio-tycho/org.yamcs.studio.core/src/main/java/org/yamcs/studio/core/web/FullProtobufChannelInitializer.java
+++ yamcs-studio-tycho/org.yamcs.studio.core/src/main/java/org/yamcs/studio/core/web/FullProtobufChannelInitializer.java
@@ -1,11 +1,5 @@
 package org.yamcs.studio.core.web;
 
-import java.io.InputStream;
-
-import org.yamcs.protobuf.Web.RestExceptionMessage;
-
-import com.google.protobuf.MessageLite;
-
 import io.netty.buffer.ByteBufInputStream;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
@@ -16,6 +10,12 @@ import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpClientCodec;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpResponseStatus;
+
+import java.io.InputStream;
+
+import org.yamcs.protobuf.Web.RestExceptionMessage;
+
+import com.google.protobuf.MessageLite;
 
 /**
  * Assumes responses are smaller than 1MB and aggregates any http content before unserializing
@@ -34,7 +34,7 @@ public class FullProtobufChannelInitializer extends ChannelInitializer<SocketCha
     protected void initChannel(SocketChannel ch) throws Exception {
         ChannelPipeline p = ch.pipeline();
         p.addLast(new HttpClientCodec());
-        p.addLast(new HttpObjectAggregator(1048576));
+        p.addLast(new HttpObjectAggregator(2097152));
         p.addLast(new SimpleChannelInboundHandler<FullHttpResponse>() {
             @Override
             public void channelRead0(ChannelHandlerContext ctx, FullHttpResponse response) throws Exception {
