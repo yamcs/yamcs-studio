d19ecfd25da4ea7b42dfb8de2729a2013be0f56f
m-sc <msc@spaceapplications.com>
2016-01-04 10:08:34 +0100

    #29: Refactor export of events using com.csvreader.CsvWriter to increase export speed.

--- yamcs-studio-tycho/org.yamcs.studio.ui.eventlog/src/main/java/org/yamcs/studio/ui/eventlog/ExportEventsHandler.java
+++ yamcs-studio-tycho/org.yamcs.studio.ui.eventlog/src/main/java/org/yamcs/studio/ui/eventlog/ExportEventsHandler.java
@@ -1,8 +1,9 @@
 package org.yamcs.studio.ui.eventlog;
 
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
@@ -17,9 +18,9 @@ import org.eclipse.ui.handlers.HandlerUtil;
 import org.yamcs.protobuf.Yamcs.Event;
 import org.yamcs.utils.TimeEncoding;
 
-public class ExportEventsHandler extends AbstractHandler {
+import com.csvreader.CsvWriter;
 
-    EventLogView eventLogView = null;
+public class ExportEventsHandler extends AbstractHandler {
 
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
@@ -27,7 +28,13 @@ public class ExportEventsHandler extends AbstractHandler {
         // get related event view
         Shell shell = HandlerUtil.getActiveShell(event);
         IWorkbenchPart part = HandlerUtil.getActivePartChecked(event);
-        this.eventLogView = (EventLogView) part;
+        EventLogView eventLogView = (EventLogView) part;
+        doExecute(eventLogView, shell);
+        return null;
+    }
+
+    public void doExecute(EventLogView eventLogView, Shell shell)
+    {
 
         // Ask for file to export
         FileDialog dialog = new FileDialog(Display.getCurrent().getActiveShell(), SWT.SAVE);
@@ -36,7 +43,7 @@ public class ExportEventsHandler extends AbstractHandler {
         System.out.println("export file choosen: " + exportFile);
         if (exportFile == null) {
             // cancelled
-            return null;
+            return;
         }
 
         // Write CSV
@@ -47,56 +54,34 @@ public class ExportEventsHandler extends AbstractHandler {
         {
             MessageDialog.openError(shell, "Export Events",
                     "Unable to perform events export.\nDetails:" + e.getMessage());
-            return null;
+            return;
         }
 
         MessageDialog.openInformation(shell, "Export Events", "Events exported successfully.");
 
-        return null;
     }
 
     private void exportEventToCsv(String exportFile, EventLogContentProvider eventLogContentProvider) throws IOException {
-
-        String fs = ","; //csvFiedSeparator
-        String fd = "\""; // csvFieldDelimiter
-        String eol = "\r\n"; // csvEndOfLine
-        String escapedFd = "'";
-        String csvHeader = "";
-        String csvBody = "";
-
-        // Build header
-        csvHeader += fd + "Sequence Number" + fd + fs;
-        csvHeader += fd + "Severity" + fd + fs;
-        csvHeader += fd + "Message" + fd + fs;
-        csvHeader += fd + "Source" + fd + fs;
-        csvHeader += fd + "Type" + fd + fs;
-        csvHeader += fd + "Reception Time" + fd + fs;
-        csvHeader += fd + "Generation Time" + fd;
-        csvHeader += eol;
-
-        Object[] events = (Object[]) eventLogContentProvider.getElements(null);
-        for (Object o : events)
-        {
-            Event event = (Event) o;
-            csvBody += fd + event.getSeqNumber() + fd + fs;
-            csvBody += fd + event.getSeverity().name() + fd + fs;
-            csvBody += fd + event.getMessage().replace(fd, escapedFd) + fd + fs;
-            csvBody += fd + event.getSource() + fd + fs;
-            if (event.getType() != null)
-                csvBody += fd + event.getType() + fd + fs;
-            else
-                csvBody += fd + fd + fs;
-            csvBody += fd + TimeEncoding.toString((event).getReceptionTime()) + fd + fs;
-            csvBody += fd + TimeEncoding.toString((event).getGenerationTime()) + fd;
-            csvBody += eol;
-        }
-        String csv = csvHeader + csvBody;
-
-        // write the file
+        char fs = '\t';
+        CsvWriter writer = null;
         File file = new File(exportFile);
-        FileWriter writer = new FileWriter(file);
-        writer.write(csv);
-        writer.flush();
-        writer.close();
+        try {
+            writer = new CsvWriter(new FileOutputStream(file), fs, Charset.forName("UTF-8"));
+            writer.writeRecord(new String[] { "Sequence Number", "Severity", "Message", "Source", "Type", "Reception Time", "Generation Time" });
+            writer.setForceQualifier(true);
+            for (Event event : eventLogContentProvider.getSortedEvents()) {
+
+                writer.writeRecord(new String[] {
+                        event.getSeqNumber() + "",
+                        event.getSeverity().name(),
+                        event.getMessage(),
+                        event.getType() != null ? event.getType() : "",
+                        TimeEncoding.toString((event).getReceptionTime()),
+                        TimeEncoding.toString((event).getGenerationTime()) });
+            }
+        } finally {
+            writer.close();
+        }
     }
+
 }
