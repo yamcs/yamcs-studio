bac6f900d20979388808d904f33c02e217806352
m-sc <msc@spaceapplications.com>
2016-04-04 15:56:57 +0200

    #41: command selection, added tree navigation based on xtce structure

--- yamcs-studio-tycho/org.yamcs.studio.core.ui/src/main/java/org/yamcs/studio/core/ui/utils/CenteredImageLabelProvider.java
+++ yamcs-studio-tycho/org.yamcs.studio.core.ui/src/main/java/org/yamcs/studio/core/ui/utils/CenteredImageLabelProvider.java
@@ -5,6 +5,7 @@ import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.widgets.TreeItem;
 
 /**
  * A table column with SWT.CENTER and just an image, will not be centered on at least Windows
@@ -22,7 +23,12 @@ public abstract class CenteredImageLabelProvider extends OwnerDrawLabelProvider 
     public void paint(Event event, Object element) {
         Image img = getImage(element);
         if (img != null) {
-            Rectangle bounds = ((TableItem) event.item).getBounds(event.index);
+
+            Rectangle bounds;
+            if (event.item instanceof TreeItem)
+                bounds = ((TreeItem) event.item).getBounds(event.index);
+            else
+                bounds = ((TableItem) event.item).getBounds(event.index);
             Rectangle imgBounds = img.getBounds();
 
             bounds.width /= 2;
--- yamcs-studio-tycho/org.yamcs.studio.ui.commanding/src/main/java/org/yamcs/studio/ui/commanding/stack/AddToStackWizardPage1.java
+++ yamcs-studio-tycho/org.yamcs.studio.ui.commanding/src/main/java/org/yamcs/studio/ui/commanding/stack/AddToStackWizardPage1.java
@@ -4,16 +4,16 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import org.eclipse.jface.layout.TableColumnLayout;
+import org.eclipse.jface.layout.TreeColumnLayout;
 import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.resource.LocalResourceManager;
 import org.eclipse.jface.resource.ResourceManager;
-import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.ColumnLabelProvider;
 import org.eclipse.jface.viewers.ColumnPixelData;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.TableViewer;
-import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.TreeViewerColumn;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerComparator;
 import org.eclipse.jface.wizard.WizardPage;
@@ -24,8 +24,8 @@ import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.TreeColumn;
 import org.yamcs.protobuf.Mdb.CommandInfo;
 import org.yamcs.protobuf.Mdb.SignificanceInfo;
 import org.yamcs.protobuf.Mdb.SignificanceInfo.SignificanceLevelType;
@@ -36,9 +36,11 @@ import org.yamcs.studio.core.ui.utils.RCPUtils;
 
 public class AddToStackWizardPage1 extends WizardPage {
 
+    public static final String COL_PATH = "XTCE Path";
     public static final String COL_SIGN = "Sig.";
     public static final String COL_QNAME = "Qualified Name";
     public static final int COLUMN_WIDTH = 10;
+    public static final int COLUMN_MAX_WIDTH = 600;
 
     private Image level1Image;
     private Image level2Image;
@@ -47,8 +49,8 @@ public class AddToStackWizardPage1 extends WizardPage {
     private Image level5Image;
 
     private StackedCommand command;
-    TableViewer commandsTable;
-    TableColumnLayout tcl;
+    TreeViewer commandsTreeTable;
+    TreeColumnLayout tcl;
 
     List<String> namespaces = new ArrayList();
 
@@ -59,14 +61,20 @@ public class AddToStackWizardPage1 extends WizardPage {
         setPageComplete(false);
     }
 
+    // Add dynamically columns for each alias of a command
     private void addAliasColumn(String namespace) {
-        TableViewerColumn aliasColumn = new TableViewerColumn(commandsTable, SWT.NONE);
+
+        TreeViewerColumn aliasColumn = new TreeViewerColumn(commandsTreeTable, SWT.NONE);
         aliasColumn.getColumn().setText(namespace);
 
         aliasColumn.setLabelProvider(new ColumnLabelProvider() {
             @Override
             public String getText(Object element) {
                 CommandInfo cmd = (CommandInfo) element;
+                if (cmd.getAbstract()) {
+                    // show a blank line if the command is abstract
+                    return "";
+                }
                 List<NamedObjectId> aliases = cmd.getAliasList();
                 for (NamedObjectId aliase : aliases) {
                     if (aliase.getNamespace().equals(namespace))
@@ -80,6 +88,7 @@ public class AddToStackWizardPage1 extends WizardPage {
 
     @Override
     public void createControl(Composite parent) {
+
         Composite composite = new Composite(parent, SWT.NONE);
         setControl(composite);
 
@@ -88,6 +97,11 @@ public class AddToStackWizardPage1 extends WizardPage {
         gl.marginWidth = 0;
         composite.setLayout(gl);
 
+        // add filter box
+        Text searchbox = new Text(composite, SWT.SEARCH | SWT.BORDER | SWT.ICON_CANCEL);
+        searchbox.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+        // build tree table
         ResourceManager resourceManager = new LocalResourceManager(JFaceResources.getResources(), composite);
         level1Image = resourceManager.createImage(RCPUtils.getImageDescriptor(AddToStackWizardPage1.class, "icons/level1s.png"));
         level2Image = resourceManager.createImage(RCPUtils.getImageDescriptor(AddToStackWizardPage1.class, "icons/level2s.png"));
@@ -95,19 +109,29 @@ public class AddToStackWizardPage1 extends WizardPage {
         level4Image = resourceManager.createImage(RCPUtils.getImageDescriptor(AddToStackWizardPage1.class, "icons/level4s.png"));
         level5Image = resourceManager.createImage(RCPUtils.getImageDescriptor(AddToStackWizardPage1.class, "icons/level5s.png"));
 
-        Text searchbox = new Text(composite, SWT.SEARCH | SWT.BORDER | SWT.ICON_CANCEL);
-        searchbox.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-
         Composite tableWrapper = new Composite(composite, SWT.NONE);
-        tcl = new TableColumnLayout();
+        tcl = new TreeColumnLayout();
         tableWrapper.setLayoutData(new GridData(GridData.FILL_BOTH));
         tableWrapper.setLayout(tcl);
 
-        commandsTable = new TableViewer(tableWrapper, SWT.SINGLE | SWT.BORDER | SWT.FULL_SELECTION | SWT.V_SCROLL | SWT.H_SCROLL);
-        commandsTable.getTable().setHeaderVisible(true);
-        commandsTable.getTable().setLinesVisible(false);
+        commandsTreeTable = new TreeViewer(tableWrapper, SWT.BORDER | SWT.V_SCROLL | SWT.H_SCROLL);
+        commandsTreeTable.getTree().setHeaderVisible(true);
+        commandsTreeTable.getTree().setLinesVisible(false);
 
-        TableViewerColumn significanceColumn = new TableViewerColumn(commandsTable, SWT.NONE);
+        // column xtce path
+        TreeViewerColumn pathColumn = new TreeViewerColumn(commandsTreeTable, SWT.NONE);
+        pathColumn.getColumn().setText(COL_PATH);
+        pathColumn.setLabelProvider(new ColumnLabelProvider() {
+            @Override
+            public String getText(Object element) {
+                CommandInfo cmd = (CommandInfo) element;
+                return cmd.getName();
+            }
+        });
+        tcl.setColumnData(pathColumn.getColumn(), new ColumnPixelData(COLUMN_WIDTH));
+
+        // column significance
+        TreeViewerColumn significanceColumn = new TreeViewerColumn(commandsTreeTable, SWT.NONE);
         significanceColumn.getColumn().setText(COL_SIGN);
         significanceColumn.getColumn().setToolTipText("Significance");
         significanceColumn.getColumn().setAlignment(SWT.CENTER);
@@ -135,18 +159,24 @@ public class AddToStackWizardPage1 extends WizardPage {
         });
         tcl.setColumnData(significanceColumn.getColumn(), new ColumnPixelData(40));
 
-        TableViewerColumn nameColumn = new TableViewerColumn(commandsTable, SWT.NONE);
+        // column qualified name
+        TreeViewerColumn nameColumn = new TreeViewerColumn(commandsTreeTable, SWT.NONE);
         nameColumn.getColumn().setText(COL_QNAME);
         nameColumn.setLabelProvider(new ColumnLabelProvider() {
             @Override
             public String getText(Object element) {
                 CommandInfo cmd = (CommandInfo) element;
+                if (cmd.getAbstract()) {
+                    // show a blank line if the command is abstract
+                    return "";
+                }
                 return cmd.getQualifiedName();
             }
         });
         tcl.setColumnData(nameColumn.getColumn(), new ColumnPixelData(COLUMN_WIDTH));
 
-        commandsTable.addSelectionChangedListener(evt -> {
+        // on item selection update significance message and page completion status
+        commandsTreeTable.addSelectionChangedListener(evt -> {
             IStructuredSelection sel = (IStructuredSelection) evt.getSelection();
             if (sel.isEmpty()) {
                 setMessage(null);
@@ -154,6 +184,7 @@ public class AddToStackWizardPage1 extends WizardPage {
             }
 
             CommandInfo cmd = (CommandInfo) sel.getFirstElement();
+
             SignificanceInfo significance = cmd.getSignificance();
             if (significance != null) {
                 StringBuilder buf = new StringBuilder();
@@ -171,32 +202,51 @@ public class AddToStackWizardPage1 extends WizardPage {
                 setMessage(null);
             }
             command.setMetaCommand(cmd);
-            setPageComplete(true);
+            setPageComplete(!cmd.getAbstract());
         });
-
-        commandsTable.setContentProvider(ArrayContentProvider.getInstance());
+        CommandTreeContentProvider commandTreeContentProvider = new CommandTreeContentProvider();
+        commandsTreeTable.setContentProvider(commandTreeContentProvider);
 
         // load command list
-        Collection<CommandInfo> nonAbstract = new ArrayList<>();
+        Collection<CommandInfo> commandInfos = new ArrayList<>();
         CommandingCatalogue.getInstance().getMetaCommands().forEach(cmd -> {
-            if (!cmd.getAbstract()) {
-                for (NamedObjectId alias : cmd.getAliasList()) {
-                    String namespace = alias.getNamespace();
-                    if (!namespaces.contains(namespace) && !namespace.startsWith("/")) {
-                        namespaces.add(namespace);
-                        addAliasColumn(namespace);
-                    }
+
+            // add aliases columns
+            for (NamedObjectId alias : cmd.getAliasList()) {
+                String namespace = alias.getNamespace();
+                if (!namespaces.contains(namespace) && !namespace.startsWith("/")) {
+                    namespaces.add(namespace);
+                    addAliasColumn(namespace);
                 }
-                nonAbstract.add(cmd);
+            }
+            commandInfos.add(cmd);
+        });
+        commandsTreeTable.setInput(commandInfos);
+        commandsTreeTable.expandAll();
+
+        // adjust columns width to content up to COLUMN_MAX_WIDTH
+        // with a small hack to display full data on the first column
+        for (TreeColumn tc : commandsTreeTable.getTree().getColumns())
+            tc.pack();
+        pathColumn.getColumn().setWidth(pathColumn.getColumn().getWidth() + 11 * commandTreeContentProvider.nbLevels);
+        for (TreeColumn tc : commandsTreeTable.getTree().getColumns()) {
+            if (tc.getWidth() > COLUMN_MAX_WIDTH)
+                tc.setWidth(COLUMN_MAX_WIDTH);
+        }
+
+        // filter
+        CommandInfoTreeViewerFilter filter = new CommandInfoTreeViewerFilter(commandTreeContentProvider);
+        commandsTreeTable.addFilter(filter);
+        searchbox.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent ke) {
+                filter.setSearchTerm(searchbox.getText());
+                commandsTreeTable.refresh();
+                commandsTreeTable.expandAll();
             }
         });
-        commandsTable.setInput(nonAbstract);
 
-        // adjust columns width to content
-        for (TableColumn tc : commandsTable.getTable().getColumns())
-            tc.pack();
-
-        commandsTable.setComparator(new ViewerComparator() {
+        commandsTreeTable.setComparator(new ViewerComparator() {
             @Override
             public int compare(Viewer viewer, Object o1, Object o2) {
                 CommandInfo c1 = (CommandInfo) o1;
@@ -205,14 +255,86 @@ public class AddToStackWizardPage1 extends WizardPage {
             }
         });
 
-        CommandInfoViewerFilter filter = new CommandInfoViewerFilter();
-        commandsTable.addFilter(filter);
-        searchbox.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyReleased(KeyEvent ke) {
-                filter.setSearchTerm(searchbox.getText());
-                commandsTable.refresh();
-            }
-        });
     }
+
+    class CommandTreeContentProvider implements ITreeContentProvider {
+
+        ArrayList<CommandInfo> commandInfos;
+        public int nbLevels = 1;
+
+        @Override
+        public void dispose() {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public Object[] getElements(Object inputElement) {
+            commandInfos = (ArrayList<CommandInfo>) inputElement;
+            ArrayList<CommandInfo> rootCommands = new ArrayList<>();
+
+            // find root commands
+            for (CommandInfo ci : commandInfos) {
+                if (!hasParent(ci))
+                    rootCommands.add(ci);
+            }
+
+            // compute number of inheritance level
+            int currentNbLevels = 1;
+            for (CommandInfo ci : commandInfos) {
+                currentNbLevels = nbParents(ci) + 1;
+                if (currentNbLevels > nbLevels)
+                    nbLevels = currentNbLevels;
+            }
+
+            return rootCommands.toArray();
+        }
+
+        private boolean hasParent(CommandInfo ci) {
+            return ci.getBaseCommand() != null && !ci.getBaseCommand().getName().equals("");
+        }
+
+        private int nbParents(CommandInfo ci) {
+            if (hasParent(ci)) {
+                CommandInfo parent = (CommandInfo) getParent(ci);
+                return nbParents(parent) + 1;
+            } else {
+                return 0;
+            }
+        }
+
+        @Override
+        public Object[] getChildren(Object parentElement) {
+            ArrayList<CommandInfo> children = new ArrayList<>();
+            CommandInfo parentCi = (CommandInfo) parentElement;
+            for (CommandInfo ci : commandInfos) {
+                if (ci.getBaseCommand().getQualifiedName().equals(parentCi.getQualifiedName()))
+                    children.add(ci);
+            }
+            return children.toArray();
+        }
+
+        @Override
+        public Object getParent(Object element) {
+            CommandInfo baseCommand = ((CommandInfo) element).getBaseCommand();
+            for (CommandInfo ci : commandInfos) {
+                if (ci.getQualifiedName().equals(baseCommand.getQualifiedName()))
+                    return ci;
+            }
+            return null;
+        }
+
+        @Override
+        public boolean hasChildren(Object element) {
+            Object[] children = getChildren(element);
+            return children != null && children.length > 0;
+        }
+
+    }
+
 }
