3de884ff05a88a8fb6eaab1ebed769a69e8c06f5
Fabian Diet <fabian.diet@spaceapplications.com>
2017-10-09 22:37:11 +0200

    Remove need to specify packets or PPs in create replay dialog

--- org.yamcs.studio.ui.archive/src/main/java/org/yamcs/studio/ui/archive/CreateReplayDialog.java
+++ org.yamcs.studio.ui.archive/src/main/java/org/yamcs/studio/ui/archive/CreateReplayDialog.java
@@ -1,7 +1,6 @@
 package org.yamcs.studio.ui.archive;
 
 import java.util.Calendar;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -10,9 +9,6 @@ import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.IMessageProvider;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.dialogs.TitleAreaDialog;
-import org.eclipse.jface.viewers.ArrayContentProvider;
-import org.eclipse.jface.viewers.CheckboxTableViewer;
-import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -23,7 +19,6 @@ import org.eclipse.swt.widgets.DateTime;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Text;
 import org.yamcs.protobuf.Rest.CreateProcessorRequest;
 import org.yamcs.protobuf.YamcsManagement.ClientInfo;
@@ -48,12 +43,6 @@ public class CreateReplayDialog extends TitleAreaDialog {
     private DateTime startTime;
     private Calendar startTimeValue;
 
-    private TableViewer packetsTable;
-    private List<String> packetsValue;
-
-    private TableViewer ppTable;
-    private List<String> ppValue;
-
     public CreateReplayDialog(Shell parentShell) {
         super(parentShell);
     }
@@ -77,58 +66,7 @@ public class CreateReplayDialog extends TitleAreaDialog {
         layout.verticalSpacing = 2;
         container.setLayout(layout);
 
-        /*
-         * PACKET TABLE
-         */
         Label lbl = new Label(container, SWT.NONE);
-        lbl.setText("Packets:");
-        GridData gd = new GridData();
-        gd.verticalAlignment = SWT.TOP;
-        lbl.setLayoutData(gd);
-
-        Composite tableWrapper = new Composite(container, SWT.NONE);
-        tableWrapper.setLayoutData(new GridData(GridData.FILL_BOTH));
-        GridLayout gl = new GridLayout();
-        gl.marginHeight = 0;
-        gl.marginWidth = 0;
-        tableWrapper.setLayout(gl);
-
-        packetsTable = CheckboxTableViewer.newCheckList(tableWrapper, SWT.V_SCROLL | SWT.BORDER);
-        packetsTable.setContentProvider(ArrayContentProvider.getInstance());
-        gd = new GridData(GridData.FILL_HORIZONTAL);
-        gd.heightHint = 5 * packetsTable.getTable().getItemHeight();
-        packetsTable.getTable().setLayoutData(gd);
-        packetsTable.setInput(packetsValue);
-        for (TableItem item : packetsTable.getTable().getItems())
-            item.setChecked(true);
-
-        /*
-         * PP Table
-         */
-        lbl = new Label(container, SWT.NONE);
-        lbl.setText("PPs:");
-        lbl.setToolTipText("Processed Parameters");
-        gd = new GridData();
-        gd.verticalAlignment = SWT.TOP;
-        lbl.setLayoutData(gd);
-
-        tableWrapper = new Composite(container, SWT.NONE);
-        tableWrapper.setLayoutData(new GridData(GridData.FILL_BOTH));
-        gl = new GridLayout();
-        gl.marginHeight = 0;
-        gl.marginWidth = 0;
-        tableWrapper.setLayout(gl);
-
-        ppTable = CheckboxTableViewer.newCheckList(tableWrapper, SWT.V_SCROLL | SWT.BORDER);
-        ppTable.setContentProvider(ArrayContentProvider.getInstance());
-        gd = new GridData(GridData.FILL_HORIZONTAL);
-        gd.heightHint = 5 * ppTable.getTable().getItemHeight();
-        ppTable.getTable().setLayoutData(gd);
-        ppTable.setInput(ppValue);
-        for (TableItem item : ppTable.getTable().getItems())
-            item.setChecked(true);
-
-        lbl = new Label(container, SWT.NONE);
         lbl.setText("Start At:");
         Composite startComposite = new Composite(container, SWT.NONE);
         RowLayout rl = new RowLayout();
@@ -181,29 +119,17 @@ public class CreateReplayDialog extends TitleAreaDialog {
         return nameValue;
     }
 
-    public void initialize(TimeInterval interval, List<String> packets, List<String> ppGroups) {
+    public void initialize(TimeInterval interval) {
         startTimeValue = TimeEncoding.toCalendar(interval.calculateStart());
         startTimeValue.setTimeZone(TimeCatalogue.getInstance().getTimeZone());
-
-        packetsValue = packets;
-        ppValue = ppGroups;
     }
 
     public CreateProcessorRequest toCreateProcessorRequest(ClientInfo ci) {
         CreateProcessorRequest.Builder resultb = CreateProcessorRequest.newBuilder()
                 .setName(name.getText())
                 .setStart(TimeEncoding.toString(TimeEncoding.fromCalendar(RCPUtils.toCalendar(startDate, startTime))))
-                .setStop(TimeEncoding.toString(TimeEncoding.MAX_INSTANT))
                 .setLoop(false)
                 .addClientId(ci.getId());
-
-        for (TableItem item : packetsTable.getTable().getItems())
-            if (item.getChecked())
-                resultb.addPacketname(item.getText());
-
-        for (TableItem item : ppTable.getTable().getItems())
-            if (item.getChecked())
-                resultb.addPpgroup(item.getText());
 
         return resultb.build();
     }
--- org.yamcs.studio.ui.archive/src/main/java/org/yamcs/studio/ui/archive/CreateReplayHandler.java
+++ org.yamcs.studio.ui.archive/src/main/java/org/yamcs/studio/ui/archive/CreateReplayHandler.java
@@ -1,15 +1,9 @@
 package org.yamcs.studio.ui.archive;
 
-import java.util.List;
-
-import javax.swing.SwingUtilities;
-
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.IWorkbenchPart;
-import org.eclipse.ui.handlers.HandlerUtil;
 import org.yamcs.studio.core.TimeInterval;
 import org.yamcs.studio.core.model.TimeCatalogue;
 
@@ -17,19 +11,13 @@ public class CreateReplayHandler extends AbstractHandler {
 
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
-        IWorkbenchPart part = HandlerUtil.getActivePartChecked(event);
-        SwingUtilities.invokeLater(() -> {
-            ArchiveView view = (ArchiveView) part;
-            long missionTime = TimeCatalogue.getInstance().getMissionTime(true);
-            missionTime -= 30 * 1000;
-            TimeInterval interval = TimeInterval.starting(missionTime);
-            List<String> packets = view.archivePanel.getSelectedPackets("tm");
-            List<String> pps = view.archivePanel.getSelectedPackets("pp");
-            Display.getDefault().asyncExec(() -> {
-                CreateReplayDialog dialog = new CreateReplayDialog(Display.getCurrent().getActiveShell());
-                dialog.initialize(interval, packets, pps);
-                dialog.open();
-            });
+        long missionTime = TimeCatalogue.getInstance().getMissionTime(true);
+        missionTime -= 30 * 1000;
+        TimeInterval interval = TimeInterval.starting(missionTime);
+        Display.getDefault().asyncExec(() -> {
+            CreateReplayDialog dialog = new CreateReplayDialog(Display.getCurrent().getActiveShell());
+            dialog.initialize(interval);
+            dialog.open();
         });
 
         return null;
