2787ab53e81131fb2b6e8bc1c7ac4b81c6bd7f40
Fabian Diet <fabian.diet@spaceapplications.com>
2017-11-11 11:21:13 +0100

    Show error popup when instance restart failed

--- bundles/org.yamcs.studio.core.ui/src/main/java/org/yamcs/studio/core/ui/RestartInstanceHandler.java
+++ bundles/org.yamcs.studio.core.ui/src/main/java/org/yamcs/studio/core/ui/RestartInstanceHandler.java
@@ -1,18 +1,35 @@
 package org.yamcs.studio.core.ui;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.handlers.HandlerUtil;
 import org.yamcs.protobuf.YamcsManagement.ClientInfo;
 import org.yamcs.studio.core.model.ManagementCatalogue;
 
 public class RestartInstanceHandler extends AbstractHandler {
+    
+    private static final Logger log = Logger.getLogger(RestartInstanceHandler.class.getName());
 
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
         ManagementCatalogue catalogue = ManagementCatalogue.getInstance();
         ClientInfo clientInfo = catalogue.getCurrentClientInfo();
-        catalogue.restartInstance(clientInfo.getInstance());
+        Shell shell = HandlerUtil.getActiveShell(event);
+        
+        String instance = clientInfo.getInstance();
+        catalogue.restartInstance(clientInfo.getInstance()).whenComplete((ret, ex) -> {
+            log.log(Level.SEVERE, "Failed to restart instance '" + instance + "'", ex);
+            Display.getDefault().asyncExec(() -> {
+                MessageDialog.openError(shell, "Failed to restart instance '" + instance + "'", ex.getMessage());
+            });
+        });
 
         return null;
     }
