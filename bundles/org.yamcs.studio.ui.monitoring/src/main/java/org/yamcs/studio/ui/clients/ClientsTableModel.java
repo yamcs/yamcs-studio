edb44cc2ab71cdc797d43af25066e34060731a4c
Fabian Diet <fabian.diet@spaceapplications.com>
2017-12-11 10:27:52 +0100

    Remove exception silencers

--- bundles/org.yamcs.studio.ui.monitoring/src/main/java/org/yamcs/studio/ui/clients/ClientsTableModel.java
+++ bundles/org.yamcs.studio.ui.monitoring/src/main/java/org/yamcs/studio/ui/clients/ClientsTableModel.java
@@ -1,62 +1,44 @@
 package org.yamcs.studio.ui.clients;
 
 import java.util.ArrayList;
-import java.util.logging.Logger;
+import java.util.List;
 
 import org.yamcs.protobuf.YamcsManagement.ClientInfo;
 
 public class ClientsTableModel {
-    private static final Logger log = Logger.getLogger(ClientsTableModel.class.getName());
-    private ArrayList<ClientInfo> clients = new ArrayList<ClientInfo>();
+
+    private List<ClientInfo> clients = new ArrayList<>();
     private ClientsTableViewer clientsTableViewer;
 
-    public ClientsTableModel(ClientsTableViewer clientsTableViewer)
-    {
+    public ClientsTableModel(ClientsTableViewer clientsTableViewer) {
         this.clientsTableViewer = clientsTableViewer;
     }
 
     public void updateClient(ClientInfo uci) {
-        try {
-            boolean found = false;
-            for (int i = 0; i < clients.size(); ++i) {
-                ClientInfo ci = clients.get(i);
-                if (ci.getId() == uci.getId()) {
-                    clients.set(i, uci);
-                    clientsTableViewer.replace(uci, i);
-                    found = true;
-                    break;
-                }
+        boolean found = false;
+        for (int i = 0; i < clients.size(); ++i) {
+            ClientInfo ci = clients.get(i);
+            if (ci.getId() == uci.getId()) {
+                clients.set(i, uci);
+                clientsTableViewer.replace(uci, i);
+                found = true;
+                break;
             }
-            if (!found) {
-                clients.add(uci);
-                clientsTableViewer.add(uci);
-            }
-        } catch (Exception e)
-        {
-            log.severe(e.toString());
+        }
+        if (!found) {
+            clients.add(uci);
+            clientsTableViewer.add(uci);
         }
 
     }
 
     public void removeClient(ClientInfo rci) {
-        try {
-            boolean found = false;
-            for (int i = 0; i < clients.size(); ++i) {
-                ClientInfo ci = clients.get(i);
-                if (ci.getId() == rci.getId()) {
-                    clients.remove(ci);
-                    clientsTableViewer.remove(ci);
-                    found = true;
-                    break;
-                }
+        for (ClientInfo ci : clients) {
+            if (ci.getId() == rci.getId()) {
+                clients.remove(ci);
+                clientsTableViewer.remove(ci);
+                break;
             }
-            if (!found) {
-                log.warning("client not found in list: " + rci);
-            }
-        } catch (Exception e)
-        {
-            log.severe(e.toString());
         }
-
     }
 }
