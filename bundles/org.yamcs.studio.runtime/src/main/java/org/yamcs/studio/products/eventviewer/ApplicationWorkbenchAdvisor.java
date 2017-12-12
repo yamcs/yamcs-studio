3779907f91006f6001f98d15fffc4182d1af0892
Fabian Diet <fabian.diet@spaceapplications.com>
2017-12-12 18:04:40 +0100

    Put products in projects with suffix folder

---
+++ bundles/org.yamcs.studio.runtime/src/main/java/org/yamcs/studio/products/eventviewer/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,18 @@
+package org.yamcs.studio.runtime;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+    @Override
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+
+    @Override
+    public String getInitialWindowPerspectiveId() {
+        return "org.yamcs.studio.runtime.perspective";
+    }
+}
