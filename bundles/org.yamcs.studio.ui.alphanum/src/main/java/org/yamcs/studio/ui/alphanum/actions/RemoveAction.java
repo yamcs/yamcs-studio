6814b9feaee59e01d5892c1ac4abbba96c896407
lddmenezes <lauradm11@gmail.com>
2018-02-06 10:15:10 +0100

    Enable remove when rows selected

--- bundles/org.yamcs.studio.ui.alphanum/src/main/java/org/yamcs/studio/ui/alphanum/actions/RemoveAction.java
+++ bundles/org.yamcs.studio.ui.alphanum/src/main/java/org/yamcs/studio/ui/alphanum/actions/RemoveAction.java
@@ -2,25 +2,52 @@ package org.yamcs.studio.ui.alphanum.actions;
 
 import java.util.List;
 
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.yamcs.protobuf.Mdb.ParameterInfo;
 import org.yamcs.studio.ui.alphanum.ParameterTableViewer;
 
 public class RemoveAction extends AlphaNumericAction {
-	
 
+    private ISelectionChangedListener listener;
+    
     public RemoveAction(final ParameterTableViewer viewer) {
-    	super("icons/elcl16/remove.png", viewer);
+        super("icons/elcl16/remove.png", viewer);
         setToolTipText("Remove");
+        setEnabled(false);
+        listener = new ISelectionChangedListener() {
+            
+            @Override
+            public void selectionChanged(SelectionChangedEvent event) {
+                if(event.getSelection().isEmpty())
+                    setEnabled(false);
+                else
+                    setEnabled(true);
+                
+            }
+        };
+        
 
     }
 
-	@Override
+    @Override
     public void run() {
-    	List<ParameterInfo> selected = (List<ParameterInfo>)viewer.getStructuredSelection().toList();
+        List<ParameterInfo> selected = (List<ParameterInfo>)viewer.getStructuredSelection().toList();
         for(ParameterInfo info : selected) {
-        	viewer.removeParameter(info);
+            viewer.removeParameter(info);
         }
     }
-	
+
+    public void setViewer(ParameterTableViewer viewer) {
+        if( getViewer() != null)
+            viewer.removeSelectionChangedListener(listener);
+        super.setViewer(viewer);
+        if( getViewer() != null)
+            viewer.addSelectionChangedListener(listener);
+        
+    }
+
+
+
 
 }
