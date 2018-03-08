997c02684a3020b6efb74e68d2357fa35b38bfe9
Fabian Diet <fabian.diet@spaceapplications.com>
2018-03-08 21:04:42 +0100

    Disable perspective switcher

--- bundles/org.yamcs.studio.editor/src/main/java/org/yamcs/studio/editor/YamcsStudioWorkbenchWindowAdvisor.java
+++ bundles/org.yamcs.studio.editor/src/main/java/org/yamcs/studio/editor/YamcsStudioWorkbenchWindowAdvisor.java
@@ -12,6 +12,8 @@ import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.ui.IMemento;
+import org.eclipse.ui.IPerspectiveDescriptor;
+import org.eclipse.ui.IPerspectiveListener;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
@@ -60,9 +62,22 @@ public class YamcsStudioWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
         configurer.setShowMenuBar(true);
         configurer.setShowCoolBar(true);
         configurer.setShowProgressIndicator(true);
-        configurer.setShowPerspectiveBar(true);
+        configurer.setShowPerspectiveBar(false);
         configurer.setShowStatusLine(true);
-        configurer.setTitle(Platform.getProduct().getName());
+        updateTitle();
+
+        configurer.getWindow().addPerspectiveListener(new IPerspectiveListener() {
+
+            @Override
+            public void perspectiveChanged(IWorkbenchPage page, IPerspectiveDescriptor perspective, String changeId) {
+                updateTitle();
+            }
+
+            @Override
+            public void perspectiveActivated(IWorkbenchPage page, IPerspectiveDescriptor perspective) {
+                updateTitle();
+            }
+        });
 
         // Workaround for text editor DND bug.
         // See http://www.eclipse.org/forums/index.php/m/333816/
@@ -127,5 +142,18 @@ public class YamcsStudioWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
         // Otherwise could have been used to prevent the part stack closing when the last
         // view is closed in runtime mode.
         return super.isDurableFolder(perspectiveId, folderId);
+    }
+
+    private void updateTitle() {
+        IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
+        String title = Platform.getProduct().getName();
+        IWorkbenchPage currentPage = configurer.getWindow().getActivePage();
+        if (currentPage != null) {
+            IPerspectiveDescriptor perspective = currentPage.getPerspective();
+            if (perspective != null) {
+                title = perspective.getLabel();
+            }
+        }
+        configurer.setTitle(title);
     }
 }
