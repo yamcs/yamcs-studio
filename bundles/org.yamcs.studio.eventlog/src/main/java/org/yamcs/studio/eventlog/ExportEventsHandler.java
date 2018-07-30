996db7be075eba977b6b1ca06cbd8662ace7118d
Fabian Diet <fabian.diet@spaceapplications.com>
2018-07-30 15:32:22 +0200

    Avoid import of csvreader

--- bundles/org.yamcs.studio.eventlog/src/main/java/org/yamcs/studio/eventlog/ExportEventsHandler.java
+++ bundles/org.yamcs.studio.eventlog/src/main/java/org/yamcs/studio/eventlog/ExportEventsHandler.java
@@ -1,9 +1,8 @@
 package org.yamcs.studio.eventlog;
 
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.util.List;
 
 import org.eclipse.core.commands.AbstractHandler;
@@ -17,9 +16,6 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.yamcs.protobuf.Yamcs.Event;
-import org.yamcs.utils.TimeEncoding;
-
-import com.csvreader.CsvWriter;
 
 public class ExportEventsHandler extends AbstractHandler {
 
@@ -51,24 +47,24 @@ public class ExportEventsHandler extends AbstractHandler {
     }
 
     private void writeEvents(File targetFile, List<Event> events) throws IOException {
-        CsvWriter writer = null;
-        try {
-            writer = new CsvWriter(new FileOutputStream(targetFile), '\t', Charset.forName("UTF-8"));
-            writer.writeRecord(new String[] { "Sequence Number", "Severity", "Message", "Source", "Type",
-                    "Reception Time", "Generation Time" });
-            writer.setForceQualifier(true);
+        try (FileWriter writer = new FileWriter(targetFile)) {
+            writer.write("Severity\tMessage\tType\tSource\tGeneration\tReception\tSequence Number\n");
             for (Event event : events) {
-                writer.writeRecord(new String[] {
-                        event.getSeqNumber() + "",
-                        event.getSeverity().name(),
-                        event.getMessage(),
-                        event.getSource() != null ? event.getSource() : "",
-                        event.getType() != null ? event.getType() : "",
-                        TimeEncoding.toString((event).getReceptionTime()),
-                        TimeEncoding.toString((event).getGenerationTime()) });
+                writer.write("" + event.getSeverity());
+                writer.write("\t");
+                writer.write(event.getMessage());
+                writer.write("\t");
+                writer.write(event.getType());
+                writer.write("\t");
+                writer.write(event.getSource());
+                writer.write("\t");
+                writer.write(event.getGenerationTimeUTC());
+                writer.write("\t");
+                writer.write(event.getReceptionTimeUTC());
+                writer.write("\t");
+                writer.write(event.getSeqNumber());
+                writer.write("\n");
             }
-        } finally {
-            writer.close();
         }
     }
 
