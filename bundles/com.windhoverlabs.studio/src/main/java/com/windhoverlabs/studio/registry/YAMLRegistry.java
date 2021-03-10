3be17fa05b2b5f69732a9bbab69de3d2b01d8773
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2021-03-10 17:46:05 -0600

    -Updated docs

--- bundles/com.windhoverlabs.studio/src/main/java/com/windhoverlabs/studio/registry/YAMLRegistry.java
+++ bundles/com.windhoverlabs.studio/src/main/java/com/windhoverlabs/studio/registry/YAMLRegistry.java
@@ -1,6 +1,11 @@
 package com.windhoverlabs.studio.registry;
 import java.util.LinkedHashMap;
 
+/**
+ * 
+ * @author lgomez
+ *The YAML Registry represents a registry(a collection of configuration) that is written to some YAML file.
+ */
 public class YAMLRegistry implements ConfigRegistry {
 
 	@Override
