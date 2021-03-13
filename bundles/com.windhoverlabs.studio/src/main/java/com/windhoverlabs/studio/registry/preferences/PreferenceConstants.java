b622f37bad842b57f7218ab46e2e30def399feed
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2021-03-12 18:44:47 -0600

    -Added default path to preference default -TODO:Make a decision about path conventions

--- bundles/com.windhoverlabs.studio/src/main/java/com/windhoverlabs/studio/registry/preferences/PreferenceConstants.java
+++ bundles/com.windhoverlabs.studio/src/main/java/com/windhoverlabs/studio/registry/preferences/PreferenceConstants.java
@@ -7,5 +7,5 @@ public class PreferenceConstants {
 	public static final String DEF_CONFIG_PATHS = "registryDirectory";
 	public static final String[]SUPPORTED_EXTENSIONS = new String[]{"*.yml;*.yaml"};
 	public static final String REGISTRY_DB = "com.windhoverlabs.studio.registryDB";
-	public static final String REGISTRY_DEFAULT_PATH = "";
+	public static final String REGISTRY_DEFAULT_PATH = "${project_loc:Displays}/Resources/definitions.yaml";
 }
