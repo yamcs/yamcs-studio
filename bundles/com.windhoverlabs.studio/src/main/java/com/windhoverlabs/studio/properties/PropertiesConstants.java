6008276a4d9f46a8f6e4600cab8bb766b99dbd2b
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2021-03-15 10:51:59 -0600

    -Removed unused PropertyPage

--- bundles/com.windhoverlabs.studio/src/main/java/com/windhoverlabs/studio/properties/PropertiesConstants.java
+++
@@ -1,9 +0,0 @@
-package com.windhoverlabs.studio.properties;
-
-public class PropertiesConstants {
-
-	public static final String DEF_CONFIG_PATHS = "registryDirectory";
-	public static final String[]SUPPORTED_EXTENSIONS = new String[]{"*.yml;*.yaml"};
-	public static final String REGISTRY_DB = "com.windhoverlabs.studio.registryDB";
-
-}
