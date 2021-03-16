4845f48cce4dc94cf015a61d01f8a8b8b444949c
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2021-03-16 16:07:54 -0500

    -Cleanup

--- bundles/com.windhoverlabs.studio/src/main/java/com/windhoverlabs/studio/registry/preferences/PreferenceInitializer.java
+++ bundles/com.windhoverlabs.studio/src/main/java/com/windhoverlabs/studio/registry/preferences/PreferenceInitializer.java
@@ -12,7 +12,7 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 * Initialize preference store Default on Activator's start() method, not here.
 	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
 	 */
 	public void initializeDefaultPreferences() {
