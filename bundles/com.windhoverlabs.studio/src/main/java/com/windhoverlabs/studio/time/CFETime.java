6396751f582d7d51f93f8d8ef008d13d3fc15d32
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2021-04-08 18:08:37 -0500

    -Initial implementation of custom epoch function -TODO:Test in Studio

--- bundles/com.windhoverlabs.studio/src/main/java/com/windhoverlabs/studio/time/CFETime.java
+++ bundles/com.windhoverlabs.studio/src/main/java/com/windhoverlabs/studio/time/CFETime.java
@@ -1,6 +1,8 @@
 package com.windhoverlabs.studio.time;
 
-import java.time.Instant; 
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit; 
 
 public class CFETime {
 
@@ -65,23 +67,88 @@ public class CFETime {
 	    return(MicroSeconds);
 
 	} /* End of CFE_TIME_Sub2MicroSecs() */
+
+	public static long secondsToMilliseconds(long seconds) 
+	{
+		return seconds * 1000;
+	}
 	
-/**
- * Return relative since the Epoch. In java this is the Unix Epoch: 1970-01-01T00:00:00Z. Very useful
- * for representing CFE relative time such as MET time.
- * @param seconds
- * @param subSeconds
- * @return
- */
- public static Instant getRelativeTime( long seconds, long subSeconds) 
-{
-	long subMicroSecs = CFE_TIME_Sub2MicroSecs(subSeconds);
+	public static long microSecondsToMilliseconds(long microSeconds) 
+	{
+		return microSeconds / 1000;
+	}
+	
+	public static long yearToMilliseconds(long years) 
+	{
+		return years * 31557600000L;
+	}
+	
+	/**
+	 * Return relative since the Epoch. In java this is the Unix Epoch: 1970-01-01T00:00:00Z. Very useful
+	 * for representing CFE relative time such as MET time.
+	 * @param seconds
+	 * @param subSeconds
+	 * @return
+	 */
+	 public static Instant getRelativeTime( long seconds, long subSeconds) 
+	{
+		long subMicroSecs = CFE_TIME_Sub2MicroSecs(subSeconds);
+			
+		//Convert time to milliseconds since that is what the Time API supports
+		long secondsMilliseconds = secondsToMilliseconds(seconds);
+		long subMilliSecs = microSecondsToMilliseconds(subMicroSecs);
 		
-	//Convert time to milliseconds since that is what the Time API supports
-	long secondsMilliseconds = seconds * 1000;
-	long subMilliSecs = subMicroSecs / 1000;
-	
-	return Instant.ofEpochMilli(secondsMilliseconds + subMilliSecs);
-}
+		return Instant.ofEpochMilli(secondsMilliseconds + subMilliSecs);
+	}
+ 
+	 /**
+	  * Return relative since the Epoch. In java this is the Unix Epoch: 1970-01-01T00:00:00Z. Very useful
+	  * for representing CFE relative time such as MET time.
+	  * @param seconds
+	  * @param subSeconds
+	  * @return
+	  */
+	  public static Instant getTimeSinceEpoch(long seconds, long subSeconds, long epochYear, long epochDay, long epochHour, long epochMinute, long epochSecond) 
+	{
+	 	long subMicroSecs = CFE_TIME_Sub2MicroSecs(subSeconds);
+	 		
+	 	//Convert time to milliseconds since that is what the Time API supports
+	 	long secondsMilliseconds = secondsToMilliseconds(seconds);
+	 	long subMilliSecs = microSecondsToMilliseconds(subMicroSecs);
+
+//		long timestamp = bornDate.getTime();
+		Calendar javaEpochCalendar = Calendar.getInstance();
+		javaEpochCalendar.setTimeInMillis(0);
+//return cal.get(Calendar.YEAR);
+		
+		System.out.println("cal-->" + javaEpochCalendar.get(Calendar.DAY_OF_YEAR));
+		
+		long epochYearDelta =  epochYear -  javaEpochCalendar.get(Calendar.YEAR);
+		long epochYearDeltaInMilliseconds = javaEpochCalendar.get(Calendar.YEAR);
+		
+		if (epochYearDelta>0) 
+		{
+			
+			epochYearDeltaInMilliseconds = yearToMilliseconds(epochYearDelta);
+		}
+		
+		long epochDayDeltaInMilliseconds = (int)TimeUnit.DAYS.toMillis(javaEpochCalendar.get(Calendar.DAY_OF_YEAR - 1) -  (epochDay-1));
+		long epochHourDeltaInMilliseconds = (int)TimeUnit.HOURS.toMillis(javaEpochCalendar.get(Calendar.HOUR_OF_DAY) -  (epochHour));
+		long epochMinuteDeltaInMilliseconds = (int)TimeUnit.MINUTES.toMillis(javaEpochCalendar.get(Calendar.MINUTE) -  (epochMinute));
+
+		
+		Instant javaEpoch = Instant.ofEpochMilli(0);
+	 	Instant newEpoch = Instant.ofEpochMilli(epochYearDeltaInMilliseconds 
+	 											+ epochDayDeltaInMilliseconds
+	 											+ epochHourDeltaInMilliseconds
+	 											+ epochMinuteDeltaInMilliseconds);
+	 	//TODO:Test in Studio.
+	 	System.out.println("newEpoch-->" + newEpoch);
+//	 	javaEpoch.until(javaEpoch, null)
+	 	
+//	 	epochYearSeconds =   31557600
+	 	
+	 	return Instant.ofEpochMilli(secondsMilliseconds + subMilliSecs);
+	}
 
 }
